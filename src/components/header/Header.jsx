3ba91d010547dff57de9276c514f424c31eeb7c9
Viren-45 <2000viren@gmail.com>
2024-03-21 22:51:28 -0400

    header updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <header>
       <div className="container header_container">
-        <h5>Hello I'm</h5>
+        <h5>Hi There. I'm</h5>
         <h1>Virendra Purohit</h1>
         <h5 className="text-light">Aspiring Developer</h5>
         <CTA />
