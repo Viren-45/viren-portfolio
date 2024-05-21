191bf4500f723e07bfb00785e9b6f86e01b24840
Viren-45 <2000viren@gmail.com>
2024-05-21 19:40:16 -0400

    Issues fixed

--- src/components/portfolio/projects.js
+++ src/components/portfolio/projects.js
@@ -1,33 +1,42 @@
-import IMG1 from "../../assets/fitness-club.png"
-import IMG2 from "../../assets/lyriks.png"
-import IMG3 from "../../assets/tesla-clone.PNG";
-import IMG4 from "../../assets/Sneaker-website.PNG";
+
+import IMG1 from "../../assets/intelliprompt.png"
+import IMG2 from "../../assets/fitness-club.png"
+import IMG3 from "../../assets/lyriks.png"
+import IMG4 from "../../assets/tesla-clone.PNG";
+import IMG5 from "../../assets/Sneaker-website.PNG";
 
 const projects = [
   {
     id: 1,
     image: IMG1,
+    title: "Intelliprompt",
+    github: "https://github.com/Viren-45/sneaker.git",
+    demo: "https://viren-intelliprompt-gwnq2qjj8-virendra-rajpurohits-projects.vercel.app/"
+  },
+  {
+    id: 2,
+    image: IMG2,
     title: "Gym Fitness Club",
     github: "https://github.com/Viren-45/sneaker.git",
     demo: "https://virendrafitnessclub.netlify.app/"
   },
   {
-    id: 2,
-    image: IMG2,
+    id: 3,
+    image: IMG3,
     title: "Spotify Clone 2.0",
     github: "https://github.com/Viren-45/sneaker.git",
     demo: "https://viren-45.github.io/sneaker/"
   },
   {
-    id: 3,
-    image: IMG3,
+    id: 4,
+    image: IMG4,
     title: "Tesla clone",
     github: "https://github.com/Viren-45/tesla-clone.git",
     demo: "https://viren-45.github.io/tesla-clone/"
   },
   {
-    id: 4,
-    image: IMG4,
+    id: 5,
+    image: IMG5,
     title: "Landing page for Website",
     github: "https://github.com/Viren-45/sneaker.git",
     demo: "https://viren-45.github.io/sneaker/"
